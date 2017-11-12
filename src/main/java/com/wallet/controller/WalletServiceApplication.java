200b1406488bb64e48e452cee9527c5c3c4edcc8
Swapnil Gaikwad <swapnilg@thoughtworks.com>
2017-11-12 12:15:08 +0530

    [Swapnil] #1 Created basic spring project

---
+++ src/main/java/com/wallet/controller/WalletServiceApplication.java
@@ -0,0 +1,10 @@
+package com.wallet.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication class WalletServiceApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run( WalletServiceApplication.class, args );
+    }
+}
---
+++ src/test/java/com/wallet/controller/WalletServiceApplicationTest.java
@@ -0,0 +1,11 @@
+package com.wallet.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WalletServiceApplicationTest {
+    @Test
+    public void hello() {
+        Assert.assertEquals( 1, 1 );
+    }
+}
