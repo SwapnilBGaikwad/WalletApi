51e74ca08ba127bc2b84447b79fb9ce5795e7b5a
Swapnil Gaikwad <swapnilg@thoughtworks.com>
2017-11-12 18:23:53 +0530

    [Swapnil] #3 Bug fixes

--- src/main/java/com/wallet/WalletServiceApplication.java
+++ src/main/java/com/wallet/WalletServiceApplication.java
@@ -3,7 +3,7 @@ package com.wallet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication class WalletServiceApplication {
+@SpringBootApplication public class WalletServiceApplication {
     public static void main( String[] args ) {
         SpringApplication.run( WalletServiceApplication.class, args );
     }
